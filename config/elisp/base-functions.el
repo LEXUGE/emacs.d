b96b0f3651f0d71469874b592312d5722f4629e2
Harry Ying <lexugeyky@outlook.com>
2020-11-27 14:37:44 +0800

    Trim trailing spaces only in prog-mode

--- config/elisp/base-functions.el
+++ config/elisp/base-functions.el
@@ -6,7 +6,8 @@
 (defun enable-trailing-whitespace ()
   "Show trailing spaces and delete on saving."
   (setq show-trailing-whitespace t)
-  (add-hook 'before-save-hook #'delete-trailing-whitespace nil t))
+  (when (derived-mode-p 'prog-mode)
+    (delete-trailing-whitespace)))
 
 (defun my-backup-enable-p (name)
   "Filter certain file backups"
