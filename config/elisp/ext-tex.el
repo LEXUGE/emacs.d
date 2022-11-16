b017985da819c67a821257d69b18305e842bd17f
Harry Ying <lexugeyky@outlook.com>
2022-11-16 19:20:20 +0000

    use emacs-pgtk in nixpkgs; add a few more latex shortcuts

--- config/elisp/ext-tex.el
+++ config/elisp/ext-tex.el
@@ -10,16 +10,22 @@
   (setq cdlatex-command-alist
       '(("sum" "Insert \\sum\\limits_{}^{}"
          "\\sum\\limits_{?}^{}" cdlatex-position-cursor nil nil t)
+	("prod" "Insert \\prod\\limits_{}^{}"
+         "\\prod\\limits_{?}^{}" cdlatex-position-cursor nil nil t)
 	("int" "Insert \\int\\limits_{}^{}"
          "\\int\\limits_{?}^{}" cdlatex-position-cursor nil nil t)
 	("tt" "Insert \\text{}"
 	 "\\text{?}" cdlatex-position-cursor nil nil t)
+	("bf" "Insert \\mathbf{}"
+	 "\\mathbf{?}" cdlatex-position-cursor nil nil t)
+	("bb" "Insert \\mathbb{}"
+	 "\\mathbb{?}" cdlatex-position-cursor nil nil t)
 	("apr" "Insert \\approx{}"
 	 "\\approx{}" cdlatex-position-cursor nil nil t)
 	("alid"       "Insert an ALIGNED environment template"
 	 "" cdlatex-environment ("aligned") t t)
-        ("prd" "Insert \\prod_{}^{}"
-         "\\prod_{?}^{}" cdlatex-position-cursor nil nil t))))
+	("eqn"       "Insert an EQUATION environment template"
+	 "" cdlatex-environment ("equation") t t))))
 
 (use-package pdf-tools
   :mode  ("\\.pdf\\'" . pdf-view-mode)
--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
             ((importer [ emacs-overlay.overlay ]
               system).emacsWithPackagesFromUsePackage {
                 config = concatEmacsConfig ./config/elisp;
-                package = (importer [ emacs-overlay.overlay ] system).pkgs.emacsPgtk;
+                package = (importer [ emacs-overlay.overlay ] system).pkgs.emacs-gtk;
                 alwaysEnsure = true;
               })) flake-utils.lib.defaultSystems)) {
                 ash-emacs-source =
