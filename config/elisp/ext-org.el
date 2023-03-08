cf3c4801043dc6aa0f9a67bc17978c2359df52f4
Harry Ying <lexugeyky@outlook.com>
2023-03-08 19:58:01 +0000

    python: init

--- config/elisp/ext-org.el
+++ config/elisp/ext-org.el
@@ -7,6 +7,11 @@
         org-agenda-files (if (file-exists-p (concat org-directory "/agenda/")) (list (concat org-directory "/agenda/"))))
   (setq org-startup-indented t)
   (setq org-enforce-todo-dependencies t) ; Enforce dependencies
+  (setq org-enforce-todo-checkbox-dependencies t) ; Enforce checkbox dependency
+  (setq org-priority-highest 1)
+  (setq org-priority-lowest 10)
+  (setq org-priority-default 3)
+  (setq org-log-done 'time) ; Log closing time
   (setq TeX-auto-untabify 't) ; Convert tabs properly into PDF
   (setq org-latex-toc-command "\\tableofcontents \\clearpage") ; Force page break after TOC
   :hook (org-mode . org-cdlatex-mode)
---
+++ config/elisp/lang-python.el
@@ -0,0 +1,7 @@
+(use-package lsp-python-ms
+  :ensure t
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-python-ms)
+                         (lsp)))
+  :init
+  (setq lsp-python-ms-executable (executable-find "python-language-server")))
